d02caaf57eea7e4a148f1c509e1cf52db1ffc85a
Paul LOISIL <fur.waz06@gmail.com>
2026-08-19 18:25:48 +0200

    Added more gait commands

--- src/tny360/modules/gait.ts
+++ src/tny360/modules/gait.ts
@@ -1,4 +1,4 @@
-import { UInt8 } from "../../core/DataTypes";
+import { Float32, UInt8 } from "../../core/DataTypes";
 import { Flag } from "../../core/Protocol";
 import { Module } from "./Module";
 
@@ -22,5 +22,45 @@ export class GaitModule extends Module {
 
     public async getType() {
         return await this.sendAction(0x01, [], [new UInt8()]).then(res => res?.[0] as GaitType);
+    }
+
+    public async setFrequency(freq_hz: number, waitResponse = false) {
+        return await this.sendAction(0x02, [new Float32(freq_hz)], [], waitResponse ? Flag.RequireAck : Flag.None);
+    }
+
+    public async getFrequency() {
+        return await this.sendAction(0x03, [], [new Float32()]).then(res => res?.[0] as number);
+    }
+
+    public async setDutyFactor(factor: number, waitResponse = false) {
+        return await this.sendAction(0x04, [new Float32(factor)], [], waitResponse ? Flag.RequireAck : Flag.None);
+    }
+
+    public async getDutyFactor() {
+        return await this.sendAction(0x05, [], [new Float32()]).then(res => res?.[0] as number);
+    }
+
+    public async setStepHeight(height_m: number, waitResponse = false) {
+        return await this.sendAction(0x06, [new Float32(height_m)], [], waitResponse ? Flag.RequireAck : Flag.None);
+    }
+
+    public async getStepHeight() {
+        return await this.sendAction(0x07, [], [new Float32()]).then(res => res?.[0] as number);
+    }
+
+    public async setStanceDepth(depth_m: number, waitResponse = false) {
+        return await this.sendAction(0x08, [new Float32(depth_m)], [], waitResponse ? Flag.RequireAck : Flag.None);
+    }
+
+    public async getStanceDepth() {
+        return await this.sendAction(0x09, [], [new Float32()]).then(res => res?.[0] as number);
+    }
+
+    public async setLegSpread(spread_m: {x: number, y: number}, waitResponse = false) {
+        return await this.sendAction(0x0A, [new Float32(spread_m.x), new Float32(spread_m.y)], [], waitResponse ? Flag.RequireAck : Flag.None);
+    }
+
+    public async getLegSpread() {
+        return await this.sendAction(0x0B, [], [new Float32(), new Float32()]).then(res => ({x: res?.[0] as number, y: res?.[1] as number}) );
     }
 }
